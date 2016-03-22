640d1722a00efd6293b82c9159dfe8ab467fee18
stefan <dev@2904.cc>
2016-03-23 00:43:11 +0100

    implemented simple r3temp tests

--- tests/test_r3temp.py
+++ tests/test_r3temp.py
@@ -9,3 +9,24 @@ def test_noerror():
         r3temp.printInfo()
     except:
         assert False
+
+
+def test_existing():
+    api = r3temp.r3temp()
+    try:
+        api.getTemp()
+        api.getTempByName('Temp@CX')
+    except:
+        assert False
+        return
+    assert True
+
+
+def test_nonexisting():
+    api = r3temp.r3temp()
+    try:
+        api.getTempByName('Temp@NonExist')
+    except:
+        assert True
+        return
+    assert False
